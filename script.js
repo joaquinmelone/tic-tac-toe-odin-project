ccb016307938c141e3584b6910e75b3a59801379
Joaquin <joaquin.melone.zarauza@gmail.com>
2023-01-10 19:52:40 -0300

    Update script.js
    -Added restart and changePlayers button and fixed the tie situation

--- script.js
+++ script.js
@@ -13,10 +13,15 @@ const Player = (name, playingPosition) => {
     return { name, turn }
 }
 
-// Global variables
+// Player's form info
 
-const player1 = Player("Jugador1", 1);
-const player2 = Player("Jugador2", 2);
+const player1Form = document.getElementById("player-name-1");
+const player2Form = document.getElementById("player-name-2");
+
+// Player's creation
+
+let player1;
+let player2;
 
 // Gameboard Object
 let GameboardObj = {
@@ -52,26 +57,151 @@ const renderGameboard = (() => {
                 GameboardObj.gameboard[i] = mark;
                 gameboardSpace[i].textContent = GameboardObj.gameboard[i];
 
-                
+                isVictory(gameboardSpace);
             }            
         })
     }
 })();
 
-/* // Check for winner or tie
+//Counter for when there's no more space on the board (the game is a tie)
+let counter = 0;
 
-function endOfTheGame() {
-    if ((GameboardObj.gameboard[0] && GameboardObj.gameboard[1] && GameboardObj.gameboard[2]) === "X" || (GameboardObj.gameboard[3] && GameboardObj.gameboard[4] && GameboardObj.gameboard[5]) === "X" || (GameboardObj.gameboard[6] && GameboardObj.gameboard[7] && GameboardObj.gameboard[8]) === "X" || (GameboardObj.gameboard[0] && GameboardObj.gameboard[4] && GameboardObj.gameboard[8]) === "X" || (GameboardObj.gameboard[2] && GameboardObj.gameboard[4] && GameboardObj.gameboard[6]) === "X") {
-        alert("player1 wins")
+// Check for winner or tie
+function isVictory(cells) {
+  counter += 1;
+
+	let combs = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let comb of combs) {
+
+		if (
+			cells[comb[0]].textContent == cells[comb[1]].textContent &&
+			cells[comb[1]].textContent == cells[comb[2]].textContent &&
+			cells[comb[0]].textContent != ''
+		) {
+            if (cells[comb[0]].textContent === "X") {
+              document.querySelector(".congratulate-winner").textContent = (`${player1.name} wins!`);
+            }
+
+            else if (cells[comb[0]].textContent === "O") {
+              document.querySelector(".congratulate-winner").textContent = (`${player2.name} wins!`);
+            }
+
+            for (let i = 0; i < 9; i++) {
+                if (GameboardObj.gameboard[i] === undefined) {
+
+                    GameboardObj.gameboard[i] = "";
+                    gameboardSpace[i].textContent = GameboardObj.gameboard[i];
+                }
+            }
+		}
+
+    else if (counter === 9) {
+      document.querySelector(".congratulate-winner").textContent = (`It's a tie!`);
+      }
+	}
+
+	return false;
+}
+
+/* PLAYER'S FORM */
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+/* RESTART AND CHANGE PLAYERS BUTTON */
+
+const changePlayers = document.getElementById("change-players-button");
+const restartButton = document.getElementById("restart-button");
+
+changePlayers.addEventListener("click", () => {
+  openModal(modal);
+  
+  for (let i = 0; i < 9; i++) {
+        GameboardObj.gameboard[i] = undefined;
+        gameboardSpace[i].textContent = GameboardObj.gameboard[i];
     }
 
-    else if ((GameboardObj.gameboard[0] && GameboardObj.gameboard[1] && GameboardObj.gameboard[2]) === "O" || (GameboardObj.gameboard[3] && GameboardObj.gameboard[4] && GameboardObj.gameboard[5]) === "O" || (GameboardObj.gameboard[6] && GameboardObj.gameboard[7] && GameboardObj.gameboard[8]) === "O" || (GameboardObj.gameboard[0] && GameboardObj.gameboard[4] && GameboardObj.gameboard[8]) === "O" || (GameboardObj.gameboard[2] && GameboardObj.gameboard[4] && GameboardObj.gameboard[6]) === "O") {
-        alert("player2 wins")
+    document.querySelector(".congratulate-winner").textContent = "";
+
+  counter = 0;
+})
+
+restartButton.addEventListener("click", () => {
+  for (let i = 0; i < 9; i++) {
+        GameboardObj.gameboard[i] = undefined;
+        gameboardSpace[i].textContent = GameboardObj.gameboard[i];
     }
 
-    else if () {
+    document.querySelector(".congratulate-winner").textContent = "";
 
-    }
-} */
+  counter = 0;
+})
+
+/* SUBMIT BUTTON */
+
+openModal(modal);
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", (e) => {
+  if (!player1Form.checkValidity() || !player2Form.checkValidity()) {
+    playerName1.reportValidity();
+    playerName2.reportValidity();
+  }
+
+  else {
+    e.preventDefault();
+
+    player1 = Player(player1Form.value, 1);
+    player2 = Player(player2Form.value, 2);
+
+    closeModal(modal);
+  }
+  
+})
 
 
